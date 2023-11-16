a27dd20862f027afc13331781b1c65a0fbe813e5
selfeki <samielfeki@hotmail.com>
2023-11-16 14:59:58 -0800

    initialize Product DB table with sample data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ProductEntity } from './product/product.entity/product.entity';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  async onModuleInit() {
+    await this.seed();
+  }
+
+  async seed() {
+    const count = await ProductEntity.count();
+    if (count === 0) {
+      // Seed sample data into Products entity if it's empty
+      const products = [
+        { name: 'Product1', price: 12.99 },
+        { name: 'Product2', price: 29.99 },
+      ];
+
+      for (const productData of products) {
+        const product = new ProductEntity();
+        Object.assign(product, productData);
+        await product.save();
+      }
+
+      console.log('Database Products table seeded with Product1 and Product2');
+    }
   }
 }
